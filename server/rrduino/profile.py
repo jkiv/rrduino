c03cc5f89449474e297edd2f9f89729a91df5080
Jon Kivinen <jon@jkiv.ca>
2012-08-17 01:09:46 -0400

    profile.py got borked in rebase. Should be good now.

--- server/rrduino/profile.py
+++ server/rrduino/profile.py
@@ -1,18 +1,26 @@
 import os
-import cPickle as pickle
+import json
 import hashlib
 import logging
 
 from . import config
 
-# TODO Use JSON for profiles (so users can edit the profiles)
+# No need to change unless you've
+# extended the use of the profile.
+DEFAULT_PROFILE = {'key':             None,
+                   'handler':         config.DEFAULT_HANDLER,
+                   'handler_options': { 'path':   None,
+                                        'create': None,
+                                        'update': None }
+                  }
 
-def filename_of(s):
+def filename_for(client_id):
     '''The filename for a profile is a hash. The ID of the client is
        used for the filename. We hash it to hide this ID. The ID is
        thus not derivable from the profile.
     '''
-    return hashlib.sha256(s).hexdigest()
+    #return hashlib.sha256(client_id).hexdigest()
+    return str(client_id) + '.json'
 
 def validate(profile):
     '''Validates a profile.
@@ -20,18 +28,17 @@ def validate(profile):
     
     # Key
     if not profile['key']:
-        raise ValueError("A pre-shared key is required.")
+        raise ValueError("A pre-shared key (\"key\") is required.")
 
-    # rrdtool database path
-    if not profile['rrd']:
-        raise ValueError("A path to an rrd database is required.")
-        
+    # Validate handler options 
+    profile['handler'].validate_options(profile['handler_options'])
+ 
 def create(filename, **kwargs):
     '''Creates a profile from **kwargs.
     '''
     
     # Get the full path of the profile
-    profile_path = os.path.join(config.PROFILE_DIR, filename_of(filename))
+    profile_path = os.path.join(config.PROFILE_DIR, filename_for(filename))
     
     # Master key is hashed for storage (same as on Arduino)
     profikwargsle['key'] = hashlib.sha256(kwargs['key']).digest()
@@ -43,20 +50,26 @@ def create(filename, **kwargs):
     
     # Write the profile to disk
     with open(profile_path, 'w') as f:
-        pickle.dump(kwargs, f)
+        json.dump(kwargs, f)
 
-def load(filename):
-    '''Loads a profile from disk.
+def load_for(client_id):
+    '''Loads a profile for a client_id located in config.PROFILE_DIR. 
+    '''
+    
+    return load(os.path.join(config.PROFILE_DIR, filename_for(client_id)))
+
+def load(profile_path):
+    '''Loads a profile from disk based on filename.
     '''
     
     # Load the default profile
-    profile = config.DEFAULT_PROFILE.copy() 
-    
-    # Get the full path of the profile
-    profile_path = os.path.join(config.PROFILE_DIR, filename_of(filename))
+    profile = DEFAULT_PROFILE.copy() 
 
     # Update the default profile with the profile loaded from disk
     with open(profile_path, 'r') as f:
-        profile.update(pickle.load(f))
+        profile.update(json.load(f))
+
+    # Validate profile
+    validate(profile)
 
     return profile
