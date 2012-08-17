b9885cb1338f60bd13139eee4c769b10b2695ccc
Jon Kivinen <jon@jkiv.ca>
2012-08-17 01:27:36 -0400

    Patched up some left over crud.

--- config.h
+++ config.h
@@ -2,10 +2,10 @@
 #define CONFIG_H
 
 #define CLIENT_MAC     {0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA}
-#define SERVER_IP      IPAddress(192,168,11,2)
+#define SERVER_IP      IPAddress(192,168,0,100)
 #define SERVER_PORT    60405
-#define CLIENT_ID      "example"
-#define CLIENT_KEY     "secret123"
+#define CLIENT_ID      "test"
+#define CLIENT_KEY     "secret"
 #define CAPTURE_DELAY_MS 60000
 
 #endif
