3ed1ceb251d2dc7ccec045544644dcc3d37ca800
Jon Kivinen <jon@jkiv.ca>
2012-09-06 20:48:56 -0400

    Added (yet untested) support for 'handler' in profiles.

--- server/rrduino/profile.py
+++ server/rrduino/profile.py
@@ -1,9 +1,11 @@
 import os
+import logging
+import inspect
 import json
 import hashlib
-import logging
 
 from . import config
+from BaseHandler import BaseHandler
 
 # No need to change unless you've
 # extended the use of the profile.
@@ -15,9 +17,7 @@ DEFAULT_PROFILE = {'key':             None,
                   }
 
 def filename_for(client_id):
-    '''The filename for a profile is a hash. The ID of the client is
-       used for the filename. We hash it to hide this ID. The ID is
-       thus not derivable from the profile.
+    '''Returns the profile filename for a given client_id
     '''
     #return hashlib.sha256(client_id).hexdigest()
     return str(client_id) + '.json'
@@ -25,43 +25,24 @@ def filename_for(client_id):
 def validate(profile):
     '''Validates a profile.
     '''
-    
+
     # Key
     if not profile['key']:
         raise ValueError("A pre-shared key (\"key\") is required.")
 
     # Validate handler options 
     profile['handler'].validate_options(profile['handler_options'])
- 
-def create(filename, **kwargs):
-    '''Creates a profile from **kwargs.
-    '''
-    
-    # Get the full path of the profile
-    profile_path = os.path.join(config.PROFILE_DIR, filename_for(filename))
-    
-    # Master key is hashed for storage (same as on Arduino)
-    profikwargsle['key'] = hashlib.sha256(kwargs['key']).digest()
-    
-    logging.info("Creating profile for {0}: {1}".format(filename, profile_path))
-    
-    # Validate the profile
-    validate(kwargs)
-    
-    # Write the profile to disk
-    with open(profile_path, 'w') as f:
-        json.dump(kwargs, f)
 
 def load_for(client_id):
     '''Loads a profile for a client_id located in config.PROFILE_DIR. 
     '''
-    
+
     return load(os.path.join(config.PROFILE_DIR, filename_for(client_id)))
 
 def load(profile_path):
     '''Loads a profile from disk based on filename.
     '''
-    
+
     # Load the default profile
     profile = DEFAULT_PROFILE.copy() 
 
@@ -69,7 +50,27 @@ def load(profile_path):
     with open(profile_path, 'r') as f:
         profile.update(json.load(f))
 
+    # Convert handler class name from string to a class
+    if isinstance(profile['handler'], string):
+        profile['handler'] = import_handler_class(profile['handler'])
+
     # Validate profile
     validate(profile)
 
     return profile
+
+
+def import_handler_class(class_name):
+    '''Tries to import rrduino.class_name and returns the handler
+       class of the same name. So, this assumes that class_name.py
+       exists in the rrduino module and class_name.py contains a
+       class of the name class_name.
+    '''
+    module = __import__('rrduino.{0}'.format(class_name), fromlist = [class_name])
+    klass = getattr(module, class_name)
+
+    # Raise an error if this isn't what we expect
+    if not inspect.isclass(klass) or not issubclass(klass, BaseHandler):
+        raise TypeError('{0} is not a {1}-derived class.'.format(class_name, BaseHandler.__name__))
+
+    return klass
